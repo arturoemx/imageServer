f5fe07116aa04d305c87796b7dc568831cf97cc6
Arturo Espinosa-Romero <arturoemx@gmail.com>
2016-12-01 17:08:47 -0600

    Se modificarion client.cpp y server.cpp para que puedan recibir la dirección y porteo en que van a operar a traves de la linea de órdenes.

--- client.cpp
+++ client.cpp
@@ -1,15 +1,26 @@
-#include "Client.h"
+#include <Client.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>  
 #include <opencv2/highgui/highgui.hpp>
 
-int main() {
+int main(int argc, char **argv)
+{
+	int port = 8888;
+	char ipAddress[64] = "127.0.0.1";
 
-	Client *client = new Client(8888, "127.0.0.1");
+	if (argc > 1)
+	{
+		strncpy (ipAddress, argv[1], 63);
+		if (argc > 2)
+			port = atoi(argv[2]);
+	}
+	
+	Client *client = new Client(port, ipAddress);
 	client->connect();
 
 	Mat frame;
-	while(true) {
+	while(true)
+	{
 		client->getFrame(frame);
 		imshow("rec", frame);
 		if(waitKey(30) >= 0) break;
